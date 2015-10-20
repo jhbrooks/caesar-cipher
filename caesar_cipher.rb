2ec83e28442865e1b7a3b021a587367ece624029
James Brooks <jhbrooks@uchicago.edu>
2015-10-20 16:42:24 -0400

    implemented caesar_cipher function

---
+++ caesar_cipher.rb
@@ -0,0 +1,18 @@
+def caesar_cipher(input_string, shift_num)
+	input_string.gsub(/[A-Za-z]/) do |letter|
+		if letter.ord >= 65 && letter.ord <= 90 #uppercase
+			adjustment = 65
+		end
+
+		if letter.ord >= 97 && letter.ord <= 122 #lowercase
+			adjustment = 97
+		end
+
+		letter.ord.-(adjustment).+(shift_num).%(26).+(adjustment).chr
+	end
+end
+
+puts caesar_cipher("Hello, world!", 2)
+puts caesar_cipher("Hello, world!", -1)
+puts caesar_cipher(".AZ.az.", 27)
+puts caesar_cipher(".AZ.az.", -28)
